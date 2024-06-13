97634b91326c438c879791c614b468c9e2e1d8c2
BalanaguYashwanth <balanaguyashwanth@gmail.com>
2024-06-13 12:41:37 +0530

    Added counter and other details in smart contract with wrapper, tests

---
+++ tests/main.spec.ts
@@ -0,0 +1,32 @@
+import { Cell, beginCell, toNano } from "@ton/core";
+import { hex } from '../build/main.compiled.json';
+import { Blockchain } from "@ton/sandbox";
+import { MainContract } from "../wrappers/MainContract";
+import "@ton/test-utils";
+
+describe('main.fc contract tests', ()=>{
+    it("our first tests", async()=>{
+        const blockchain = await Blockchain.create();
+        const codeCell = await Cell.fromBoc(Buffer.from(hex, 'hex'))[0];
+        const nameCell = beginCell().storeBuffer(Buffer.from('{name:yash, age:20}', 'utf-8')).endCell();
+        const senderWallet = await blockchain.treasury('sender');
+        const initWallet = await blockchain.treasury('initWallet')
+        const config = {
+            address: senderWallet.address,
+            counter: 1,
+            name: nameCell,
+        }
+        const createConfig = await MainContract.createFromConfig(config, codeCell);
+        const myContract = await blockchain.openContract(createConfig);
+        
+        await myContract.sendIncrementCounter(senderWallet.getSender(), toNano("1"), 1, 1, initWallet.address);
+
+        const data = await myContract.getData();
+        const decodeNameCell = data.name
+        const nameSlice = decodeNameCell.beginParse();
+        const nameBytes = nameSlice.preloadBuffer(nameSlice.remainingBits / 8); // Preload the buffer from the slice
+        const name = Buffer.from(nameBytes).toString('utf-8'); // Convert buffer to string
+        console.log('name-->', name)
+        expect(data.counter).toBe(3)
+    })
+})
---
+++ wrappers/MainContract.ts
@@ -0,0 +1,39 @@
+import { Address, Contract, Cell, contractAddress, beginCell, ContractProvider, SendMode } from "@ton/core";
+
+export type createConfigTypes = {
+    address: Address,
+    counter: number,
+    name: Cell,
+}
+
+const MainContractData = (config: createConfigTypes) => {
+    return beginCell().storeAddress(config.address).storeUint(config.counter, 32).storeRef(config.name).endCell();
+}
+
+export class MainContract implements Contract{
+    constructor(
+        readonly address: Address, readonly init?: {code: Cell, data: Cell}
+    ){}
+
+    static createFromConfig(config: createConfigTypes, code: Cell, workchain = 0){
+        const data = MainContractData(config);
+        const init = {code, data}
+        const address = contractAddress(workchain,init);
+
+        return new MainContract(address, init)
+    }
+
+
+    async sendIncrementCounter(provider: ContractProvider, sender: any ,value: bigint, op: number, incrementBy: number, ownerAddres: Address){
+        await provider.internal(sender,{
+            value,
+            sendMode: SendMode.PAY_GAS_SEPARATELY,
+            body: beginCell().storeUint(op, 32).storeUint(incrementBy, 32).storeAddress(ownerAddres).endCell(),
+        })
+    }
+
+    async getData(provider: ContractProvider){
+        const {stack} = await provider.get('get_contract_latest_counter', []);
+        return {counter: stack.readNumber(), address: stack.readAddress(), name: stack.readCell()}
+    }
+}
